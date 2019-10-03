c29d6a2c2ef290475755547327020b57ef88b375
Pagos3DGit <pagos3d@gmail.com>
2019-10-03 21:19:22 +0300

    camera works

---
+++ Source/MainUI.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+
+import numpy as np 
+import cv2
+cap = cv2.VideoCapture(1)
+
+window = Tk()
+window.title("HaraKiri")
+
+# this is the window width/height
+window.geometry('600x350')
+cv2.imshow('window ', window(cap.read()) )
+
+lbl = Label(window, text = "HaraKiri")
+
+
+lbl.grid(column = 0, row =0)
+window.mainloop()
